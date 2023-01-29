06b36fc1876d9a3339abcd285d83042b5b244cad
fluorjo <fluorjo@naver.com>
2023-01-29 11:32:04 +0900

    1-색상 상수 지정

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xff22A45D);
+//글자 색상
+const BODY_TEXT_COLOR = Color(0xff868686);
+//텍스트필드 배경  색상
+const INPUT_BG_COLOR = Color(0xffFBFBFB);
+//텍스트필드 테투리  색상
+const INPUT_BORDER_COLOR = Color(0xffF3F2F2);
