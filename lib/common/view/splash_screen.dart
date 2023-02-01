612e87c4785a0eed0320881c83eb3f3318697be0
fluorjo <fluorjo@naver.com>
2023-02-01 23:35:30 +0900

    splash screen

---
+++ lib/common/view/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:inf_cf/common/layout/default_layout.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: Column(children: const []),
+    );
+  }
+}
